b787270ae64ce8f3a0e1ac2edcc7e2ec04be4163
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-04-02 14:10:09 +0600

    3rd ways use to options "bmiValue ?? 0.0"

--- BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
+++ BMI-Calculator-LayoutPractice/Models/CalculatorBrain.swift
@@ -6,18 +6,15 @@ struct CalculatorBrain {
     var bmiValue: Float?
     
     func getBMIValue() -> String {
-        if let safeBMI = bmiValue {
-            let bmiValueTo1DecimalPlace = String(format: "%.1f", safeBMI)
+        
+            let bmiValueTo1DecimalPlace = String(format: "%.1f", bmiValue ?? 0.0)
             return bmiValueTo1DecimalPlace
-        } else {
-            return "0.0"
+            
+        }
+        
+        mutating func calculateBMI(_ height: Float, _ weight: Float) {
+            bmiValue = weight / pow(height, 2)
+            
         }
         
     }
-    
-    mutating func calculateBMI(_ height: Float, _ weight: Float) {
-        bmiValue = weight / pow(height, 2)
-        
-    }
-    
-}
